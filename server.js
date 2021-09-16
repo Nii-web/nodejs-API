4ec047cd1d9bb2e3592aab49e00ac22ed1fdff11
Nii-web <69145017+Nii-web@users.noreply.github.com>
2021-09-16 08:36:25 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// parse requests of content-type: application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the homepage" });
+});
+
+require("../CRUD/routes/customer.routes")(app);
+
+
+// set port, listen for requests
+app.listen(8000, () => {
+  console.log("Server is running on port 8000.");
+});
